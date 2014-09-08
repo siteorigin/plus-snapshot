3b58675b51caf6abdd0c88c6b49a6fbbb7cafcae
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-09-08 19:05:38 +0000

    Initial commit of Snapshot Plus.

---
+++ plugins/plus-snapshot/trunk/plus-snapshot.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Snapshot Plus
+Description: Extensions and enhancements to the Snapshot WordPress theme.
+Version: trunk
+Author: Greg Priday
+Author URI: http://siteorigin.com
+Plugin URI: http://siteorigin.com/theme/snapshot/
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+*/
+
+function snapshot_plus_extras_premium_folder(){
+	return plugin_dir_url(__FILE__);
+}
+
+function snapshot_plus_init(){
+
+	if( get_option('template') != 'snapshot' || is_dir( get_template_directory() . '/premium/' ) ) {
+		// Disable the plugin
+	}
+
+	if( defined( 'SITEORIGIN_THEME_VERSION' ) ) {
+		// We're using Snapshot and that it's definitely the SiteOrigin version
+
+		add_filter('siteorigin_extras_premium_root_uri', 'snapshot_plus_init');
+
+		include plugin_dir_path(__FILE__) . '/customizer/customizer.php';
+		include plugin_dir_path(__FILE__) . '/ajax-comments/ajax-comments.php';
+		include plugin_dir_path(__FILE__) . '/css/css.php';
+		include plugin_dir_path(__FILE__) . '/widgets/widgets.php';
+
+		include plugin_dir_path(__FILE__) . '/main.php';
+
+		// Add the updates plugin
+		include plugin_dir_path(__FILE__) . '/wp-updates-plugin.php';
+		new WPUpdatesPluginUpdater_675( 'http://wp-updates.com/api/2/plugin', plugin_basename(__FILE__));
+	}
+}
+add_action('init', 'snapshot_plus_init', 1);
